9fe0e5cbbb9df616fc6d8e40ad83c41a8b0d68fe
Samira Campos <samiradivergente@gmail.com>
2026-05-03 21:56:40 -0300

    Ejercicio 02

--- Dsw2026Ej8/EJ02.cs
+++ Dsw2026Ej8/EJ02.cs
@@ -4,7 +4,21 @@ using System.Text;
 
 namespace Dsw2026Ej8
 {
-    internal class EJ02
+    public class problema2
     {
+        public string CrearResumenVenta (long productCode, string productDescription, int quantity, decimal uniPrice)
+        {
+            decimal total = 0;
+            if (quantity > 0)
+            {
+                total = quantity * uniPrice;
+            } 
+            else
+            {
+                total = 0;
+            }
+            var venta = new { Code = productCode, Description = productDescription, Quantity = quantity, Total = total };
+            return $"{venta.Code}-{venta.Description}-{venta.Total}";
+        }
     }
 }
