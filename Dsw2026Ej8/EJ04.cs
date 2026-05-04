e1275d9bb70d4cd49356fde12d94a99b06d50cc5
Samira Campos <samiradivergente@gmail.com>
2026-05-03 21:58:27 -0300

    Ejercicio 04

--- Dsw2026Ej8/EJ04.cs
+++ Dsw2026Ej8/EJ04.cs
@@ -4,7 +4,29 @@ using System.Text;
 
 namespace Dsw2026Ej8
 {
-    internal class EJ04
+    internal class Problema4
     {
+        public double CalcularPromedio (int? nota1, int? nota2, int? nota3) 
+         {
+            double suma = 0;
+            int aux = 0;
+
+            int?[] notas = { nota1, nota2, nota3 };
+
+            foreach (var nota in notas)
+            {
+                if (nota.HasValue && nota.Value >= 0 && nota.Value <= 10)
+                {
+                    suma += nota.Value;
+                    aux+= 1;
+                }
+            }
+            if (aux == 0)
+            {
+                return 0;
+            }
+
+            return suma / aux;
+        }
     }
 }
